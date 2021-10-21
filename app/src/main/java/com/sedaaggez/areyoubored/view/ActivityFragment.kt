67bae17e1096dba132951455a05c0665a42112f2
sedaaggez <sedaggez@gmail.com>
2021-10-21 22:05:55 +0300

    create model (Bored)

--- app/src/main/java/com/sedaaggez/areyoubored/view/ActivityFragment.kt
+++ app/src/main/java/com/sedaaggez/areyoubored/view/ActivityFragment.kt
@@ -1,18 +1,19 @@
-package com.sedaaggez.areyoubored
+package com.sedaaggez.areyoubored.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sedaaggez.areyoubored.R
 
-class FilterFragment : Fragment() {
+class ActivityFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_filter, container, false)
+        return inflater.inflate(R.layout.fragment_activity, container, false)
     }
 
 }
