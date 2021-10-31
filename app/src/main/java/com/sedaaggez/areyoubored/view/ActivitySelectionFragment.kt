fb04043eab208f055940da8d051daa7954f2e16f
sedaaggez <sedaggez@gmail.com>
2021-10-31 13:47:35 +0300

    set onClick navigation

--- app/src/main/java/com/sedaaggez/areyoubored/view/ActivitySelectionFragment.kt
+++ app/src/main/java/com/sedaaggez/areyoubored/view/ActivitySelectionFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.sedaaggez.areyoubored.R
+import kotlinx.android.synthetic.main.fragment_activity_selection.*
+import kotlinx.android.synthetic.main.fragment_welcome.*
+import kotlinx.android.synthetic.main.fragment_welcome.buttonYes
 
 class ActivitySelectionFragment : Fragment() {
 
@@ -14,6 +18,20 @@ class ActivitySelectionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_activity_selection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonRandom.setOnClickListener {
+            val action = ActivitySelectionFragmentDirections.actionActivitySelectionFragmentToActivityFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+        buttonFilter.setOnClickListener {
+            val action = ActivitySelectionFragmentDirections.actionActivitySelectionFragmentToFilterFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 
 }
--- app/src/main/java/com/sedaaggez/areyoubored/view/WelcomeFragment.kt
+++ app/src/main/java/com/sedaaggez/areyoubored/view/WelcomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.sedaaggez.areyoubored.R
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 class WelcomeFragment : Fragment() {
 
@@ -14,6 +16,14 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonYes.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToActivitySelectionFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
